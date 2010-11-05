685210e0b80f15d9fb02c720bcba14ff73204363
Roger Smith <roger.smith@ceryx.com>
2010-11-04 21:46:33 -0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 JoinedJourneys::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
 
-  get "pages/contact"
+  root :to => 'pages#home'
 
-  get "pages/about"
+  #get "pages/home"
+
+  #get "pages/contact"
+
+  #get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
