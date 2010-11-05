025f6d328c68d535857e10892aecb33dfe7cfb8a
Roger Smith <roger.smith@ceryx.com>
2010-11-04 20:00:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoinedJourneys::Application.config.secret_token = 'a3c98fc0e73e36dca911aab61c61f0d505ef053fe69f31335ed4ce27140947e5a8e7853573d1669086f160cd136de41ab99e600e33d21d8ee60e7928296ee799'
